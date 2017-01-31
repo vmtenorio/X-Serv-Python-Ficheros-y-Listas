f35100799ffdaf28c9cc51e711c23e124a2fbb95
Victor Manuel Tenorio Gomez <tenorio@gamma15.aulas.gsyc.urjc.es>
2017-01-31 11:54:27 +0100

    Hecho el ejercicio 13.4 de ficheros

---
+++ fich.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+fich = open("/etc/passwd", "r")
+
+users = fich.readlines()
+
+for user in users:
+    tokens = user.split(':')
+    print(tokens[0] + " usa la shell: " + tokens[-1], end='')
